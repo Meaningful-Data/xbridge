b8022a28c5736348ac42397185a3b05c12382306
javier.hernandez <73304358+javihern98@users.noreply.github.com>
2025-02-20 19:47:21 +0100

    Updated file to main.py

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ from time import time
 
 from xbridge.converter import Converter
 
-if __name__ == "__main__":
-    # INPUT_PATH_3_3 = Path(__file__).parent / "tests" / "test_files" / "sample_3_3"
+
+def main():
     INSTANCE_PATH = Path.cwd() / "input" / "dora_sample.xbrl"
-    # INSTANCE_PATH = Path(__file__).parent / "tests" / "test_files" / "sample_3_2_phase1"
-    # / "test1_in.xbrl"
 
     start = time()
     converter = Converter(INSTANCE_PATH)
@@ -21,3 +19,6 @@ if __name__ == "__main__":
     print(f"Time to initialize: {initial - start}")
     print(f"Time to convert: {end - initial}")
     print(f"Total time: {end - start}")
+
+if __name__ == "__main__":
+    main()
