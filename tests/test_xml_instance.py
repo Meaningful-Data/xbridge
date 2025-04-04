6ec92098f031187d3aec2668388f6d5081bdc378
Guillermo <guillermo.garcia@meaningfuldata.eu>
2025-04-04 12:36:46 +0200

    Added two simple tests to improve code coverage

--- tests/test_xml_instance.py
+++ tests/test_xml_instance.py
@@ -5,7 +5,7 @@ Tests for xml_instance module
 import pytest
 from lxml import etree
 
-from xbridge.xml_instance import Fact, FilingIndicator
+from xbridge.xml_instance import Fact, FilingIndicator, Instance
 
 
 @pytest.fixture
@@ -72,3 +72,11 @@ def test_fact_repr(fact):
         f"Fact(metric={fact.metric}, value=100, decimals=2, context=context1, unit=unit1)"
     )
     assert repr(fact) == expected_repr
+
+def test_no_input_path():
+    with pytest.raises(ValueError, match="Must provide a path to XBRL file."):
+        Instance()
+
+def test_invalid_path_type():
+    with pytest.raises(TypeError, match="Unsupported type for 'path' argument."):
+        Instance(0)
